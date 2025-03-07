ca535332718d5389a68135263788a94af42bd0c2
Siddhi Jain <siddhiyjain@gmail.com>
2025-03-07 17:25:19 +0100

    adding logic

---
+++ modes/mode_chrpos37.py
@@ -0,0 +1,5 @@
+def run(args):
+    """Handles Mode 2 logic."""
+    print(f"Mode 1: Processing {args.input} -> {args.output}")
+    if args.chr_col_b37:
+        print(f"chr_col_b37: {args.chr_col_b37}")
---
+++ modes/mode_chrpos38.py
@@ -0,0 +1,5 @@
+def run(args):
+    """Handles Mode 3 logic."""
+    print(f"Mode 1: Processing {args.input} -> {args.output}")
+    if args.chr_col_b38:
+        print(f"chr_col_b38: {args.chr_col_b38}")
