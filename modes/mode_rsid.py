ecc90c0f3fca57f98947ebfcc26345c4c9e28cae
Siddhi Jain <siddhiyjain@gmail.com>
2025-03-10 16:08:21 +0100

    update gitignore

--- modes/mode_rsid.py
+++ modes/mode_rsid.py
@@ -1,8 +1,14 @@
 import pandas as pd
 import sqlite3
+from cli import logger
 
 
 def run(args):
     """Handles Mode "rsid" logic."""
-    df = pd.read_csv(args.input)
+    try:
+        df = pd.read_csv(args.input)
+        logger.info("Input file read successfully.")
+    except Exception as e:
+        logger.error(f"An error has occured: {e}")
+    
     gtex_file =sqlite3.connect("data/GTEX_v10.sqlite")
