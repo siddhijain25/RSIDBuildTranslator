a1b2c6ed4470a812342899653d1a4ec8d1f9e718
Siddhi Jain <siddhiyjain@gmail.com>
2025-03-10 09:49:56 +0100

    Update cli.py

--- cli.py
+++ cli.py
@@ -14,14 +14,21 @@ def create_parser():
         choices=["rsid", "chrpos37", "chrpos38"],
         help="Mode of operation: rsid or chrpos37 or chrpos38 (Based on what you have available in your dataset)",
         required=True,
+        type=str,
     )
-    parser.add_argument("-i", "--input", help="Input file with path", required=True)
+    parser.add_argument(
+        "-i", "--input", help="Input file with path", required=True, type=str
+    )
 
     parser.add_argument(
-        "-o", "--output", help="Name of output file with path", required=True
+        "-o", "--output", help="Name of output file with path", required=True, type=str
     )
     parser.add_argument(
-        "-d", "--database", help="Name of database to use (gtex or ukb)", required=True
+        "-d",
+        "--database",
+        help="Name of database to use (gtex or ukb)",
+        required=True,
+        type=str,
     )
     return parser
 
@@ -31,7 +38,11 @@ def create_mode_specific_args(parser, args):
 
     if args.mode == "rsid":
         parser.add_argument(
-            "-rs", "--rsid_col", help="Name of column with rsids", required=True
+            "-rs",
+            "--rsid_col",
+            help="Name of column with rsids",
+            required=True,
+            type=str,
         )
     elif args.mode == "chrpos37":
         parser.add_argument(
@@ -39,12 +50,14 @@ def create_mode_specific_args(parser, args):
             "--chr_col_b37",
             help="Name of column with chromosome in build GRCh37",
             required=True,
+            type=str,
         )
         parser.add_argument(
             "-pos37",
             "--pos_col_b37",
             help="Name of column with position in build GRCh37",
             required=True,
+            type=str,
         )
     elif args.mode == "chrpos38":
         parser.add_argument(
@@ -52,11 +65,13 @@ def create_mode_specific_args(parser, args):
             "--chr_col_b38",
             help="Name of column with chromosome in build GRCh38",
             required=True,
+            type=str,
         )
         parser.add_argument(
             "-pos38",
             "--pos_col_b38",
             help="Name of column with position in build GRCh38",
             required=True,
+            type=str,
         )
     return parser
