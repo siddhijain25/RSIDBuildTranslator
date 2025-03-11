1bdd647068ca1e2818c9a3d4e9e1ba29d35cd85f
Siddhi Jain <siddhiyjain@gmail.com>
2025-03-11 10:24:15 +0100

    mode rsid run function

--- modes/mode_rsid.py
+++ modes/mode_rsid.py
@@ -1,15 +1,26 @@
 import pandas as pd
 import sqlite3
 from cli import logger
-from utils import read_input_file
+from utils import read_input_file, load_gtex_data, get_query
+import re
 
 
 def run(args):
     """Handles Mode "rsid" logic."""
 
     input_data = read_input_file(args.input)
-    
-    cursor = conn.cursor()
-    cursor.execute(query, rsid_batch)
-    results = cursor.fetchall()
-    conn.close()
+    gtex_con, gtex_cur = load_gtex_data()
+
+    input_data.columns = [
+        re.sub(r"[^a-zA-Z0-9\_ ]", "", col) for col in input_data.columns
+    ]
+    ids_to_search = input_data[args.rsid_col].tolist()
+
+    query = get_query(GTEx_lookup, ids_to_search, rsid_dbSNP155)
+
+    gtex_cur.execute(query)
+    results = gtex_cur.fetchall()
+    gtex_con.close()
+
+    for row in results:
+        print(row)
--- utils.py
+++ utils.py
@@ -5,7 +5,8 @@ from cli import logger
 
 def read_input_file(path):
     try:
-        df = pd.read_csv(path)
+        df = pd.read_table(path, sep=None, engine='python')
+        
         if df.empty:
             logger.warning("Input file is empty.")
         elif len(df.columns) < 2:
@@ -20,14 +21,15 @@ def read_input_file(path):
 
 def load_gtex_data():
     try:
-        gtex_con = sqlite3.connect("data/GTEX_v10.sqlite")
+        gtex_con = sqlite3.connect("data/GTEX_v10.db")
         gtex_cur = gtex_con.cursor()
         logger.info("GTEx database read successfully.")
         return gtex_con, gtex_cur
     except Exception as e:
         logger.error(f"An error has occured while reading the GTEx database: {e}")
         return None, None
-    
+
+
 def get_query(database, ids_to_search, lookup_column):
     # Create the SQL query dynamically
     query = f"""
@@ -37,3 +39,25 @@ def get_query(database, ids_to_search, lookup_column):
     return query
 
 
+# ###########
+# import sqlite3
+
+# # Connect to the database (replace 'GTEX_file.db' with your actual database path)
+# conn = sqlite3.connect("GTEx_v10.db")
+
+# # Sample list of RSIDs (same as `rsid_batch` in R)
+# rsid_batch = ["rs554008981", "rs28507908", "rs1557427439"]
+
+# # Create the SQL query dynamically
+# query = f"""
+# SELECT * FROM GTEx_lookup
+# WHERE rsid_dbSNP155 IN ({', '.join(['?']*len(rsid_batch))})
+# """
+# cursor = conn.cursor()
+# cursor.execute(query, rsid_batch)
+# results = cursor.fetchall()
+# conn.close()
+
+# # Print the results
+# for row in results:
+#     print(row)
