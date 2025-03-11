4ce4f5f568977938942cccd1217ab1116ebacd90
Siddhi Jain <siddhiyjain@gmail.com>
2025-03-11 15:43:04 +0100

    Update utils.py

--- utils.py
+++ utils.py
@@ -56,7 +56,9 @@ def query_to_df(query, ids_to_search, cur, input_data, rsid_col, lookup_column):
         tmp = {columns[index]: column for index, column in enumerate(value)}
         results.append(tmp)
 
-    final_df=pd.merge(input_data,pd.DataFrame(results), how='left', left_on=rsid_col, right_on=lookup_column)
+    results_df=pd.DataFrame(results)
+
+    final_df=pd.merge(input_data,results, how='left', left_on=rsid_col, right_on=lookup_column).drop(lookup_column,1)
     return final_df
 
 
